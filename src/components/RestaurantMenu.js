a71df8dea75ebea763ffafb243ac7cdf44d86366
kalyonnii <mudhiiguubbakalyonnii@gmail.com>
2024-02-21 21:52:11 +0530

    api issue resolved

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -30,24 +30,24 @@ const RestaurantMenu = () => {
 
   if (resInfo == null) return <Shimmer />;
 
-  const { itemCards } =
-    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
+  // const { itemCards } =
+  //   resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
   // console.log(itemCards);
   // console.log(
-  //   resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card
-  //     ?.itemCards
+  //   resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card?.itemCards
   // );
   //console.log(resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards);
 
   const categories =
-    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
       (c) =>
         c.card?.card?.["@type"] ==
         "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory" 
     );
   //console.log(categories)
+  //console.log(resInfo)
   const { name, cuisines, costForTwoMessage } =
-    resInfo?.cards[0]?.card?.card?.info;
+    resInfo?.cards[2]?.card?.card?.info;
 
   return (
     <div className="menu text-center">
